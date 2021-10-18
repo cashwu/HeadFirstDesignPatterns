6dc7faf0c9350529a571a4a29139b4b47a299caf
cash <cash@cashwu.com>
2021-10-18 22:46:42 +0800

    observer

--- ObserverPattern/Program.cs
+++ ObserverPattern/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ObserverPattern
 {
@@ -6,7 +7,99 @@ namespace ObserverPattern
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var subject = new MySubject();
+
+            var observerA = new ObserverA();
+            var observerB = new ObserverB();
+
+            subject.Register(observerA);
+            subject.Register(observerB);
+
+            subject.StateChange(1);
+
+            subject.Remove(observerA);
+
+            subject.StateChange(2);
         }
+    }
+
+    public class MySubject : ISubject
+    {
+        private readonly List<IMyObserver> _observers;
+
+        public MySubject()
+        {
+            _observers = new List<IMyObserver>();
+        }
+
+        public int State { get; private set; }
+
+        public void Register(IMyObserver observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void Remove(IMyObserver observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        public void Notify()
+        {
+            foreach (var observer in _observers)
+            {
+                observer.Update(this);
+                // observer.Update(State);
+            }
+        }
+
+        public void StateChange(int state)
+        {
+            State = state;
+
+            Notify();
+        }
+    }
+
+    public interface IMyObserver
+    {
+        // pass data
+        void Update(ISubject subject);
+        // void Update(int state);
+    }
+
+    public interface ISubject
+    {
+        void Register(IMyObserver observer);
+
+        void Remove(IMyObserver observer);
+
+        void Notify();
+    }
+
+    public class ObserverB : IMyObserver
+    {
+        public void Update(ISubject subject)
+        {
+            Console.WriteLine($"{nameof(ObserverB)} notify state change - {((MySubject) subject).State}");
+        }
+
+        // public void Update(int state)
+        // {
+        //     Console.WriteLine($"{nameof(ObserverB)} notify state change - {state}");
+        // }
+    }
+
+    public class ObserverA : IMyObserver
+    {
+        public void Update(ISubject subject)
+        {
+            Console.WriteLine($"{nameof(ObserverA)} notify state change - {((MySubject) subject).State}");
+        }
+
+        // public void Update(int state)
+        // {
+        //     Console.WriteLine($"{nameof(ObserverA)} notify state change - {state}");
+        // }
     }
 }
